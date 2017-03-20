8b674439ddb1eabc2b0871a0458d2e72c9b69638
Micah McKinnon <micahmckinnon@Micahs-MBP-8.local>
2017-03-20 18:13:39 -0500

    Added Framework Program

---
+++ StudentAdvisementSystem/src/java/edu/uco/hsung/UserBean.java
@@ -0,0 +1,27 @@
+package edu.uco.hsung;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.security.Principal;
+import javax.annotation.PostConstruct;
+import javax.faces.context.FacesContext;
+
+@Named(value = "userBean")
+@SessionScoped
+public class UserBean implements Serializable {
+
+    private String username;
+
+    @PostConstruct
+    public void init() {
+        FacesContext fc = FacesContext.getCurrentInstance();
+        Principal p = fc.getExternalContext().getUserPrincipal();
+        username = p.getName();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+}
